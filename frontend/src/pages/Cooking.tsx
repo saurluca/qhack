b964fc9405540bb55d896c92c76b369c35a23da8
Firestar99 <firestar99@sydow.cloud>
2025-04-23 22:22:16 +0200

    remove last header mention

--- frontend/src/pages/Cooking.tsx
+++ frontend/src/pages/Cooking.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
+import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import { createClient } from '@supabase/supabase-js';
 
